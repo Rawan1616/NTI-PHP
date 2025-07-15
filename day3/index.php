9e5632c158f3a78ac55ff4dec7c7a182b0886230
rawan saleh <galal.rawan@icloud.com>
2025-07-15 19:33:37 +0300

    intro php and sum and division

---
+++ day3/index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>My First Web Page</title>
+
+    </head>
+    <body>
+        <?php 
+        echo "Hello, World!<br>";
+        print "rawan saleh <br>";
+       
+        $n1 = 10;
+        $n2 = 20;
+        $n3 = $n1 + $n2;
+        $n4 = $n1 / $n2;
+        echo "The sum of $n1 and $n2 is $n3 <br>";
+        echo "The division of $n1 and $n2 is $n4 <br >";
+
+        ?>
+</body>
+</html>
